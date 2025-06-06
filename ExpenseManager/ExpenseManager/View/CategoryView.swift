c55e0531792c90d8f212ce15a485802c8fce5e2c
Sergey Ivanov <ivanovsergey36@hotmail.com>
2025-06-07 01:41:38 +0300

    Extracted ListView and AddCategoryView into separate subviews

--- ExpenseManager/ExpenseManager/View/CategoryView.swift
+++ ExpenseManager/ExpenseManager/View/CategoryView.swift
@@ -20,50 +20,7 @@ struct CategoryView: View {
             VStack {
                 SearchBar(searchText: $viewModel.searchText)
                 ZStack {
-                    List(viewModel.filteredCategories) { category in
-                        HStack(spacing: 16) {
-                            Image(systemName: category.icon)
-                                .foregroundColor(.blue)
-                                .font(.title2)
-                                .frame(width: 28, height: 28)
-                            Text(category.title)
-                                .fontWeight(.light)
-                                .foregroundStyle(.primary)
-                        }
-                        .background(Color(uiColor: .systemBackground))
-                        .swipeActions {
-                            Button(role: .destructive) {
-                                if let index = viewModel.categories.firstIndex(where: { $0.id == category.id }) {
-                                    
-                                    MainScreenViewModel.shared.transactions.removeAll { $0.category.title == viewModel.categories[index].title }
-                                    
-                                    viewModel.categories.remove(at: index)
-                                }
-                                
-                            } label: {
-                                Label("Удалить", systemImage: "trash")
-                            }
-                        }
-                    }
-                    .listStyle(.plain)
-                    .toolbar {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Button {
-                                // Действие для кнопки Edit
-                            } label: {
-                                Text("Править")
-                            }
-                        }
-                        
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button {
-                                isAddNewCategoryShow = true
-                            } label: {
-                                Image(systemName: "plus")
-                                    .font(.title2)
-                            }
-                        }
-                    }
+                    ListView(viewModel: viewModel, isAddNewCategoryShow: $isAddNewCategoryShow)
                     if viewModel.filteredCategories.isEmpty {
                         Text("Ничего не найдено")
                             .foregroundColor(.gray)
@@ -73,79 +30,7 @@ struct CategoryView: View {
             }
             .sheet(isPresented: $isAddNewCategoryShow) {
                 NavigationView {
-                    Form {
-                        TextField("Введите название категории", text: $title)
-                            .padding([.top, .bottom], 8)
-                        
-                        ScrollView {
-                            VStack(alignment: .leading, spacing: 28) {
-                                ForEach(viewModel.iconSections) { section in
-                                    VStack(alignment: .leading, spacing: 16) {
-                                        Text(section.title)
-                                            .frame(maxWidth: .infinity, alignment: .leading)
-                                            .fontWeight(.regular)
-                                                .foregroundStyle(.secondary)
-                                                .padding(12)
-                                                .background(
-                                                    RoundedRectangle(cornerRadius: 10)
-                                                        .fill(Color.gray.opacity(0.1))
-                                                )
-
-                                        ScrollView(.horizontal, showsIndicators: false) {
-                                            HStack(spacing: 16) {
-                                                ForEach(section.icons, id: \.self) { icon in
-                                                    Button {
-                                                        if selectedIcon == icon {
-                                                            selectedIcon = ""
-                                                        } else {
-                                                            selectedIcon = icon
-                                                        }
-                                                    } label: {
-                                                        Image(systemName: icon)
-                                                            .font(.title)
-                                                            .frame(width: 64, height: 64)
-                                                            .background(selectedIcon == icon ? Color.gray.opacity(0.15) : Color.clear)
-                                                            .clipShape(RoundedRectangle(cornerRadius: 12))
-                                                            .foregroundColor(.blue)
-                                                    }
-                                                    .buttonStyle(.plain)
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        .padding([.top, .bottom], 12)
-                    }
-                    .toolbar {
-                        ToolbarItem(placement: .topBarLeading) {
-                            Button {
-                                isAddNewCategoryShow = false
-                                title = ""
-                                selectedIcon = ""
-                            } label: {
-                                Text("Отмена").foregroundStyle(.red)
-                            }
-                        }
-                        
-                        ToolbarItem(placement: .topBarTrailing) {
-                            Button {
-                                let category = Category(title: title, icon: selectedIcon)
-                                ChartPieViewModel.shared.setColor(for: category.title, color: ChartPieViewModel.shared.generateRandomColor())
-                                viewModel.categories.append(category)
-                                title = ""
-                                selectedIcon = ""
-                                isAddNewCategoryShow = false
-                            } label: {
-                                Text("Сохранить")
-                            }
-                            .disabled(title.isEmpty || selectedIcon.isEmpty)
-                        }
-                    }
-                    .toolbarBackground(.white, for: .navigationBar)
-                    .toolbarBackground(.visible, for: .navigationBar)
+                    AddCategoryView(viewModel: viewModel, selectedIcon: $selectedIcon, title: $title, isAddNewCategoryShow: $isAddNewCategoryShow)
                 }
             
             }
@@ -157,3 +42,137 @@ struct CategoryView: View {
     CategoryView()
 }
 
+struct AddCategoryView: View {
+    
+    @ObservedObject var viewModel: CategoryViewModel
+    
+    @Binding var selectedIcon: String
+    @Binding var title: String
+    @Binding var isAddNewCategoryShow: Bool
+    
+    var body: some View {
+        Form {
+            TextField("Введите название категории", text: $title)
+                .padding([.top, .bottom], 8)
+            
+            ScrollView {
+                VStack(alignment: .leading, spacing: 28) {
+                    ForEach(viewModel.iconSections) { section in
+                        VStack(alignment: .leading, spacing: 16) {
+                            Text(section.title)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .fontWeight(.regular)
+                                .foregroundStyle(.secondary)
+                                .padding(12)
+                                .background(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .fill(Color.gray.opacity(0.1))
+                                )
+                            
+                            ScrollView(.horizontal, showsIndicators: false) {
+                                HStack(spacing: 16) {
+                                    ForEach(section.icons, id: \.self) { icon in
+                                        Button {
+                                            if selectedIcon == icon {
+                                                selectedIcon = ""
+                                            } else {
+                                                selectedIcon = icon
+                                            }
+                                        } label: {
+                                            Image(systemName: icon)
+                                                .font(.title)
+                                                .frame(width: 64, height: 64)
+                                                .background(selectedIcon == icon ? Color.gray.opacity(0.15) : Color.clear)
+                                                .clipShape(RoundedRectangle(cornerRadius: 12))
+                                                .foregroundColor(.blue)
+                                        }
+                                        .buttonStyle(.plain)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            
+            .padding([.top, .bottom], 12)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    isAddNewCategoryShow = false
+                    title = ""
+                    selectedIcon = ""
+                } label: {
+                    Text("Отмена").foregroundStyle(.red)
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    let category = Category(title: title, icon: selectedIcon)
+                    ChartPieViewModel.shared.setColor(for: category.title, color: ChartPieViewModel.shared.generateRandomColor())
+                    viewModel.categories.append(category)
+                    title = ""
+                    selectedIcon = ""
+                    isAddNewCategoryShow = false
+                } label: {
+                    Text("Сохранить")
+                }
+                .disabled(title.isEmpty || selectedIcon.isEmpty)
+            }
+        }
+    }
+}
+struct ListView: View {
+    
+    @ObservedObject var viewModel: CategoryViewModel
+    @Binding var isAddNewCategoryShow: Bool
+    
+    var body: some View {
+        List(viewModel.filteredCategories) { category in
+            HStack(spacing: 16) {
+                Image(systemName: category.icon)
+                    .foregroundColor(.blue)
+                    .font(.title2)
+                    .frame(width: 28, height: 28)
+                Text(category.title)
+                    .fontWeight(.light)
+                    .foregroundStyle(.primary)
+            }
+            .background(Color(uiColor: .systemBackground))
+            .swipeActions {
+                Button(role: .destructive) {
+                    if let index = viewModel.categories.firstIndex(where: { $0.id == category.id }) {
+                        
+                        MainScreenViewModel.shared.transactions.removeAll { $0.category.title == viewModel.categories[index].title }
+                        
+                        viewModel.categories.remove(at: index)
+                    }
+                    
+                } label: {
+                    Label("Удалить", systemImage: "trash")
+                }
+            }
+        }
+        .listStyle(.plain)
+        .toolbar {
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    // Действие для кнопки Edit
+                } label: {
+                    Text("Править")
+                }
+            }
+            
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    isAddNewCategoryShow = true
+                } label: {
+                    Image(systemName: "plus")
+                        .font(.title2)
+                }
+            }
+        }
+    }
+}
