973787c1ac92bf87fe7094dc0d3e6374bea2a543
Sergey Ivanov <ivanovsergey36@hotmail.com>
2025-06-07 01:50:41 +0300

    Refactored code

--- ExpenseManager/ExpenseManager/View/AddExpenseView.swift
+++ ExpenseManager/ExpenseManager/View/AddExpenseView.swift
@@ -12,7 +12,6 @@ struct AddExpenseView: View {
     @ObservedObject private var viewModel: CategoryViewModel = CategoryViewModel.shared
     
     @Environment(\.presentationMode) var presentationMode
-    
     @State private var amount = ""
     @State private var selectedCategory: Category?
     
@@ -22,30 +21,11 @@ struct AddExpenseView: View {
                 VStack {
                     SearchBar(searchText: $viewModel.searchText)
                     Form {
-                        TextField("Сумма", text: $amount)
-                            .keyboardType(.decimalPad)
-                            .padding(8)
+                        TextFieldView(amount: $amount)
                         CategoryGridView(selectedCategory: $selectedCategory)
                             .padding(.top, 12)
                     }
                     .opacity(viewModel.filteredCategories.isEmpty ? 0 : 1)
-                    .onChange(of: amount) { _, newValue in
-                        let filtered = newValue.filter { "0123456789.".contains($0) }
-                        
-                        // Проверяем количество точек (должна быть не более одной)
-                        let dotCount = filtered.filter { $0 == "." }.count
-                        if dotCount > 1 {
-                            amount.removeLast()
-                        }
-                        
-                        // Ограничение на два знака после точки
-                        if let dotIndex = filtered.firstIndex(of: ".") {
-                            let afterDot = filtered[dotIndex...].dropFirst() // Символы после точки
-                            if afterDot.count > 2 {
-                                amount.removeLast()
-                            }
-                        }
-                    }
                     .toolbar {
                         ToolbarItem(placement: .navigationBarLeading) {
                             Button("Отмена") {
@@ -57,18 +37,11 @@ struct AddExpenseView: View {
                         ToolbarItem(placement: .navigationBarTrailing) {
                             
                             Button("Добавить") {
-                                if let category = selectedCategory {
-                                    let transaction = Transaction(amount: -Int(amount)!, category: category)
-                                    MainScreenViewModel.shared.transactions.append(transaction)
-                                }
-                                viewModel.searchText = ""
-                                presentationMode.wrappedValue.dismiss()
+                                addTransaction()
                             }
                             .disabled(amount.isEmpty || selectedCategory == nil)
                         }
                     }
-                    .toolbarBackground(.white, for: .navigationBar)
-                    .toolbarBackground(.visible, for: .navigationBar)
                 }
                 if viewModel.filteredCategories.isEmpty {
                     Text("Ничего не найдено")
@@ -77,6 +50,15 @@ struct AddExpenseView: View {
                 }
             }
         }
+    }
+    
+    func addTransaction() {
+        if let category = selectedCategory {
+            let transaction = Transaction(amount: -Int(amount)!, category: category)
+            MainScreenViewModel.shared.transactions.append(transaction)
+        }
+        viewModel.searchText = ""
+        presentationMode.wrappedValue.dismiss()
     }
     
     func categoryChecker(_ amount: String, category: Category) -> Int {
